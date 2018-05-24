82008ebc50dd3af82609b5166314911dbf5f97dd
ryan <11066986@qq.com>
2018-05-24 18:07:59 +0800

    homework

--- fifth_module/第一章_HTML_CSS/homework_chouti/static/js/sub.js
+++ fifth_module/第一章_HTML_CSS/homework_chouti/static/js/sub.js
@@ -1,7 +1,4 @@
 $(function () {
-
-
-
     //点赞功能
     $('.recommend').click(function () {
         pressTimes = $(this).children('b').text();
@@ -15,6 +12,8 @@ $(function () {
 
         commentTimes = $(this).parent().siblings('.comments-box-packup').children('ul').children().length;
         $(this).parent().siblings('.comments-box-packup').children('.count-times').html(commentTimes);
+
+        $(this).children('b').html(commentTimes);
 
         //评论次数
         // commentTimes = $(this).children('b').text();
@@ -39,33 +38,61 @@ $(function () {
             })
 
         }
-        //书写评论
-        $('.comment-submit').click(function () {
-            //获取textarea里面的值
-            var commtenText = $('.comment-reply').val();
-            //创建li标签
-            var $createLi = $('<li></li>');
-            if(commtenText){
-                $createLi.html(commtenText);
-                // console.log(commtenText)
-                $(this).siblings('ul').append($createLi);
-                $('.comment-reply').val('');
-                //更新评论功能
-                commentTimes = $(this).siblings('ul').children('li').length;
-                $(this).siblings('.count-times').html(commentTimes)
 
-            }else {
-                return false;
-            }
-        });
 
 
 
     });
     
+    //书写评论
+    $('.comment-submit').click(function () {
+        //获取textarea里面的值
+        var commtenText = $('.comment-reply').val();
+        //创建li标签
+        var $createLi = $('<li></li>');
+        if(commtenText){
+            $createLi.html(commtenText);
+            // console.log(commtenText)
+            $(this).siblings('ul').append($createLi);
+            $('.comment-reply').val('');
+            //更新评论功能
+            commentTimes = $(this).siblings('ul').children('li').length;
+            $(this).siblings('.count-times').html(commentTimes);
+            $(this).parent().siblings('.part2').children('.discuss').children('b').html(commentTimes)
+
+        }else {
+            return false;
+        }
+    });
 
 
-    //统计评论次数
+    //登录 注册
+
+    $('.login_register').on('click','a',function () {
+        $('.login-register').css('display','block');
+        var $ChooseLi = $('.cd-switcher').children('li');
+        var $ChooseDiv = $('.cd-user-modal-container').children('div');
+        $('.cd-switcher').on('click','li',function () {
+            $(this).addClass('active').siblings('li').removeClass('active');
+            index = $(this).index($ChooseLi);
+            $ChooseDiv.eq(index).show().siblings('div').hide();
+
+
+        })
+    });
+
+    $('.cd-user-modal-container').click(function () {
+        return false
+    });
+    $('.cd-user-modal').click(function () {
+        return false
+    });
+    
+    $('.login-register').click(function () {
+        $('.login-register').css('display','none');
+    })
+
+
 
 
 
