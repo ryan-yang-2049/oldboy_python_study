8ccd4a606e4809437b88a46726b0a2f7ed4a6e5a
ryan <11066986@qq.com>
2018-06-18 22:56:45 +0800

    http

---
+++ sixth_module/WEB框架开发/01-JDserver.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+__title__ = '01-JDserver.py'
+__author__ = 'ryan'
+__mtime__ = '2018/6/17'
+"""
+
+import  socket
+
+sock = socket.socket()
+sock.bind(("127.0.0.1",8801))
+sock.listen(5)
+
+while True:
+	# conn 客户端套接字
+	conn,addr = sock.accept()
+
+	data = conn.recv(1024)
+	print("data:",data)
+	# HTTP/1.1 200 OK  相应首行
+	# 读取html文件
+	# POST 请求才有请求头数据
+	with open('login.html','r') as f:
+		data = f.read()
+	conn.send(("HTTP/1.1 200 OK\r\n\r\n%s"%data).encode('utf8'))
+	conn.close()
+
+
+
+
+
+
