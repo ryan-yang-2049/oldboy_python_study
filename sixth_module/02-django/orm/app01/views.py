bebfb959ff2e8e840a002646927bd8ab2938cbca
ryan <11066986@qq.com>
2018-06-25 23:23:34 +0800

    django 表操作notes

--- sixth_module/02-django/orm/app01/views.py
+++ sixth_module/02-django/orm/app01/views.py
@@ -10,15 +10,15 @@ def index(request):
 	#添加表记录
 
 	# 方式一：
-	# book_obj = Book(id=1,title="python红宝书",price=100,pub_date="2012-12-12",publish="人民出版社")
-	# book_obj.save()
+	book_obj1 = Book(id=1,title="python红宝书",price=100,pub_date="2012-12-12",publish="人民出版社")
+	book_obj1.save()
 
 	#方式二：create返回值就是当前生成的对象记录
 
-	book_obj = Book.objects.create(title="java",price=200,pub_date="2013-01-13",publish="人民出版社")
+	book_obj2 = Book.objects.create(title="java",price=200,pub_date="2013-01-13",publish="人民出版社")
 
-	print(book_obj.title)
-	print(book_obj.price)
-	print(book_obj.publish)
+	print(book_obj2.title)
+	print(book_obj2.price)
+	print(book_obj2.publish)
 
 	return HttpResponse("OK")
