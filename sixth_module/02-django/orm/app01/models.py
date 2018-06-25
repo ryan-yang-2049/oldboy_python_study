e164d963cea3274513635f1711ac6e5ed8521fe4
ryan <11066986@qq.com>
2018-06-25 17:39:25 +0800

    django 视图层

--- sixth_module/02-django/orm/app01/models.py
+++ sixth_module/02-django/orm/app01/models.py
@@ -1,10 +1,5 @@
 from django.db import models
-
-
 # Create your models here.
-
-
-
 class Book(models.Model):
      id=models.AutoField(primary_key=True)
      title=models.CharField(max_length=32)
