f83f77fd223fbb480fa41ecf2886117525aca490
ryan <11066986@qq.com>
2018-11-23 18:30:26 +0800

    update mzitu

--- seventh_module/爬虫/5.scrapy/project_11_meizitu/project_11_meizitu/spiders/meizitu.py
+++ seventh_module/爬虫/5.scrapy/project_11_meizitu/project_11_meizitu/spiders/meizitu.py
@@ -7,13 +7,20 @@ from scrapy.spiders import CrawlSpider, Rule
 class MeizituSpider(CrawlSpider):
 	name = 'meizitu'
 	# allowed_domains = ['www.mzitu.com']
-	start_urls = ['https://www.mzitu.com/158446']
-	link = LinkExtractor(allow=r'www.mzitu.com/158446/\d+')
+	start_urls = ['https://www.mzitu.com/']
+	link = LinkExtractor(allow=r'https://www.mzitu.com/page/\d+/')
 	rules = (
-		Rule(link, callback='parse_item', follow=True),
+		Rule(link, callback='parse_item', follow=False),
 	)
 
 	def parse_item(self, response):
-		li_list = response.xpath('//div[@class="main-image"]/p/a/img/@src').extract_first()
-		print(li_list)
+		link_url = response.xpath('//*[@id="pins"]/li')
+		# print("link_url==================>",link_url)
+		for link in link_url:
+			link_son_url = link.xpath('./a/@href').extract_first()
+			print("link_son_url==================>",link_son_url)
+
+
+		# li_list = response.xpath('//div[@class="main-image"]/p/a/img/@src').extract_first()
+		# print("===========>",li_list)
 		# https://i.meizitu.net/2018/11/13a05.jpg
