cb171345e17f89b87c498c47b2f97088c4e6d2a6
ryan <11066986@qq.com>
2019-02-23 13:38:37 +0800

    查看与编辑和删除

---
+++ seventh_module/CRM/45.CRM开发之私户编辑和删除/luffy_crm/web/views/public_customer.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+__title__ = 'public_customer.py'
+__author__ = 'yangyang'
+__mtime__ = '2019-02-21'
+"""
+from django.conf.urls import url
+from django.utils.safestring import mark_safe
+from django.shortcuts import HttpResponse, render
+from django.db import transaction
+
+
+from stark.service.v1 import StarkHandler, StarkModelForm, get_choice_text, get_m2m_text, Option
+from web import models
+
+
+class PublicCustomerModelForm(StarkModelForm):
+	class Meta:
+		model = models.Customer
+		exclude = ['consultant', ]
+
+
+class PublicCustomerHandler(StarkHandler):
+
+	def display_record(self, obj=None, is_header=None):
+		if is_header:
+			return '跟进记录'
+
+		record_url = self.reverse_commons_url(self.get_url_name('record_view'), pk=obj.pk)
+		return mark_safe('<a href="%s">查看跟进</a>' % record_url)
+
+	list_display = [StarkHandler.display_checkbox, 'name', 'qq', get_choice_text('状态', 'status'),
+	                get_choice_text('性别', 'gender'),
+	                get_m2m_text('咨询的课程', 'course'), display_record]
+
+	def get_queryset(self, request, *args, **kwargs):
+
+		return self.model_class.objects.filter(consultant__isnull=True)
+
+	model_form_class = PublicCustomerModelForm
+
+	def extra_urls(self):
+		patterns = [
+			url(r'^record/(?P<pk>\d+)/$', self.wrapper(self.record_view),
+			    name=self.get_url_name('record_view'))
+		]
+
+		return patterns
+
+	def record_view(self, request, pk):
+		"""
+		查看跟进记录视图
+		:param request:
+		:param pk:
+		:return:
+		"""
+		record_list = models.ConsultRecord.objects.filter(customer_id=pk)
+
+		return render(request, 'record_view.html', {'record_list': record_list})
+
+	def action_multi_apply(self, request, *args, **kwargs):
+		"""
+		批量申请到私户
+		:param request:
+		:return:
+		"""
+		"""
+		# 基本实现--
+		current_user_id = 6
+
+		# 客户ID 列表
+		pk_list = request.POST.getlist('pk')
+
+		# 将选中的客户更新到我的私户(consultant=当前用户)
+		models.Customer.objects.filter(id__in=pk_list,status=2,consultant__isnull=True).update(consultant_id=current_user_id)
+		"""
+		current_user_id = request.session['user_info']['id']
+		pk_list = request.POST.getlist('pk')
+		private_customer_count = models.Customer.objects.filter(consultant_id=current_user_id, status=2).count()
+		# 私户个数的限制
+		if private_customer_count + len(pk_list) > 150:
+			return HttpResponse('做人不要太贪心，私户中已有%s个客户,最多只能申请 %s' % (private_customer_count, 150 - private_customer_count))
+
+		# 数据库中加锁
+
+		flag = False
+		with transaction.atomic():  # 事务
+			# 在数据库中加锁
+			# 防止多个用户同时提交一个相同的客户到自己的私户
+			origin_queryser = models.Customer.objects.filter(id__in=pk_list, status=2,
+			                                                 consultant__isnull=True).select_for_update()
+			if len(origin_queryser) == len(pk_list):
+				models.Customer.objects.filter(id__in=pk_list, status=2, consultant__isnull=True).update(
+					consultant_id=current_user_id)
+				flag = True
+		if not flag:
+			return HttpResponse('手速太慢了，选中的客户已被其他人申请走')
+
+	action_multi_apply.text = "申请到私户"
+
+	action_list = [action_multi_apply, ]
+
+	search_list = ['name__contains', 'status__contains', ]
+	search_group = [
+		Option('status'),
+		Option('course')
+	]
---
+++ seventh_module/CRM/45.CRM开发之私户编辑和删除/luffy_crm/web/views/userinfo.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# __title__ = 'userinfo.py'
+# __author__ = 'YangYang'
+# __mtime__ = '2019.02.21'
+
+from django.shortcuts import HttpResponse, redirect, render
+from django.utils.safestring import mark_safe
+from django.conf.urls import url
+
+from stark.service.v1 import StarkHandler, get_choice_text, Option
+from web import models
+from web.forms.user_form import UserInfoChangeModelForm, UserInfoAddModelForm, ResetPasswordForm
+
+
+class UserInfoHandler(StarkHandler):
+	def display_reset_pwd(self, obj=None, is_header=None):
+		if is_header:
+			return "重置密码"
+		reset_url = self.reverse_commons_url(self.get_url_name('reset_pwd'), pk=obj.pk)
+		return mark_safe('<a href="%s">重置密码</a>' % reset_url)
+
+	list_display = ['nickname', 'name', get_choice_text('性别', 'gender'), 'phone', 'email', 'depart', display_reset_pwd]
+
+	def get_model_form_class(self, is_add=False):
+		"""
+		定制添加和编辑页面
+		:param is_add:
+		:return:
+		"""
+		if is_add:
+			return UserInfoAddModelForm
+		return UserInfoChangeModelForm
+
+	def reset_password(self, request, pk):
+		"""
+		重置密码的视图函数
+		:param request:
+		:param pk:
+		:return:
+		"""
+		userinfo_object = models.UserInfo.objects.filter(id=pk).first()
+		if not userinfo_object:
+			return HttpResponse("用户不存在，无法进行重置！")
+		if request.method == "GET":
+			form = ResetPasswordForm()
+			return render(request, 'stark/change.html', {'form': form})
+		form = ResetPasswordForm(data=request.POST)
+		if form.is_valid():
+			userinfo_object.password = form.cleaned_data['password']
+			userinfo_object.save()
+			return redirect(self.reverse_list_url())
+		return render(request, 'stark/change.html', {'form': form})
+
+	def extra_urls(self):
+		patterns = [
+			url(r'^reset/password/(?P<pk>\d+)/$', self.wrapper(self.reset_password),
+			    name=self.get_url_name('reset_pwd'))
+		]
+
+		return patterns
+
+	# 模糊搜索
+	search_list = ['nickname__contains', 'name__contains']
+
+	# 组合搜索
+	search_group = [
+		Option(field='gender'),
+		Option(field='depart', is_multi=True),
+	]
